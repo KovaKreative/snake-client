692298b726cb84c83140df060b04434415113bf2
KovaKreative <eric.kovalevskyy@gmail.com>
2023-03-15 13:57:29 -0400

    Set up the user input and enabled quitting of the game

--- client.js
+++ client.js
@@ -6,18 +6,23 @@ const connect = function() {
     host: '165.227.47.243',
     port: 50541
   });
-  
+
   conn.setEncoding('utf8');
 
   conn.on('data', data => {
-    console.log(data);
+    console.log("data");
   });
 
   conn.on('connect', () => {
     console.log("Connected to server!");
     conn.write("Name: EK♪");
+    // setInterval(() => {
+    //   const directions = ["Move: up", "Move: down", "Move: left", "Move: right"];
+    //   const index = Math.floor(directions.length * Math.random());
+    //   conn.write(directions[index]);
+    // }, 100);
   });
-  
+
   return conn;
 };
 
--- play.js
+++ play.js
@@ -1,3 +1,23 @@
+const { stdin } = require('process');
 const connect = require('./client');
 
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log("Quit game");
+    process.exit();
+  }
+};
+
+setupInput();
+
+stdin.on('data', handleUserInput);
